34e28bfc7ab8c90d4d114f1f4b0ad0c59b33c6f7
Bez Blazen <BezBlazen@gmail.com>
2025-11-03 04:20:14 +0500

    Projects page. Project view.

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../../models/project.model';
+import { FormGroup, FormsModule } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions, FormlyModule } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [    
+    FormsModule,
+    FormlyModule,
+  ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
-
+  @Input() project?: Project;
+  form = new FormGroup({});
+  model = this.project;
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'id',
+      type: 'input',
+      props: {
+        label: 'Id',
+        disabled: true
+      }
+    },
+    {
+      key: 'title',
+      type: 'input',
+      props: {
+        label: 'Title',
+        required: true,
+        errorTitle: '6-64 chars',
+      }
+    },
+    {
+      key: 'createdAt',
+      type: 'input',
+      props: {
+        label: 'Created at',
+        disabled: true
+      }
+    },
+  ];
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Project } from '../../models/project.model';
+import { ProjectComponent } from "../project/project.component";
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  // --------------------------------
+  // [var] Projects
+  project?: Project;
+  projects: Project[] = [];
+  // [var] Projects
+  // --------------------------------
+  constructor(
+    private dataService: DataService
+  ) {
+    this.dataService.projects$.subscribe(projects => {
+      this.projects = projects;
+      if (this.project == null && projects != null && projects.length > 0) {
+        this.project = projects[0];
+      }
+    });
+  }
+  setProject(row: Project) {
+    this.project = { ...row };
+  }
 }
